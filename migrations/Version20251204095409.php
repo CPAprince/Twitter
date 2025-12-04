262fecb045afda985f12a569d45c61ee54519bb6
Artem Danylov <a.danylov39@gmail.com>
2025-12-04 12:24:20 +0200

    feat(migration): SCRUM-2 Generate a migration for `HealthCheckReport`

---
+++ migrations/Version20251204095409.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251204095409 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE health_check_reports (status ENUM(\'ok\') NOT NULL, created_at DATETIME NOT NULL, id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE health_check_reports');
+    }
+}
