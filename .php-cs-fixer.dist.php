3d68b0b95cb628930835eb79e84bf14a1e746e0a
Artem Danylov <a.danylov39@gmail.com>
2025-12-05 14:39:52 +0200

    ci(workflow): SCRUM-5 Devise CI workflow into separate files
    - Move build, test, and deploy jobs into individual workflows
      for better clarity.
    
    - Fix Dockerfile syntax error to success CI.
    
    - Do not fix `config/reference.php` with `php-cs-fixer`,
      it is an auto-generated file and on every event it will
      rollback to the previous, "nonfixed" state.
      Eventually, it will cause linting CI to fall.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,15 +1,19 @@
 <?php
 
-$finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
-    ->exclude('var')
-;
+$finder = new PhpCsFixer\Finder()
+    ->in([
+        __DIR__.'/bin',
+        __DIR__.'/src',
+    ])
+    ->name('*.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
 
-return (new PhpCsFixer\Config())
+return new PhpCsFixer\Config()
     ->setRules([
         '@Symfony' => true,
         'strict_comparison' => true,
         'declare_strict_types' => ['preserve_existing_declaration' => false],
     ])
-    ->setFinder($finder)
-;
+    ->setRiskyAllowed(true)
+    ->setFinder($finder);
