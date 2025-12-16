e25b15539306cf39d15242a020471102b779f634
Artem Danylov <145837590+adanyl0v@users.noreply.github.com>
2025-12-16 23:14:48 +0200

    style(cs-fixer): SCRUM-51 Normalize formatting in PHP-CS-Fixer config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,27 +5,27 @@ declare(strict_types=1);
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
-$finder = (new Finder())
+$finder = new Finder()
     ->in([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-        __DIR__ . '/config',
-        __DIR__ . '/bin',
-        __DIR__ . '/tools',
+        __DIR__.'/src',
+        __DIR__.'/tests',
+        __DIR__.'/config',
+        __DIR__.'/bin',
+        __DIR__.'/tools',
     ])
     ->notName('reference.php')
     ->name('*.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return (new Config())
+return new Config()
     ->setRiskyAllowed(true)
     ->setUsingCache(true)
-    ->setCacheFile(__DIR__ . '/var/.php-cs-fixer.cache')
+    ->setCacheFile(__DIR__.'/var/.php-cs-fixer.cache')
     ->setRules([
         '@Symfony' => true,
-        'strict_param'         => true,
-        'psr_autoloading'      => true,
+        'strict_param' => true,
+        'psr_autoloading' => true,
         'declare_strict_types' => true,
     ])
     ->setFinder($finder);
