db30758e0bfb6a1021ea2f49f2e12156f81916b5
Artem Danylov <145837590+adanyl0v@users.noreply.github.com>
2025-12-16 23:23:32 +0200

    feat(iam): SCRUM-51 Add user creation API endpoint

---
+++ src/IAM/UI/REST/Controller/CreateUserController.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Twitter\IAM\UI\REST\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Twitter\IAM\Domain\User\Model\Email;
+use Twitter\IAM\Domain\User\Model\PasswordHash;
+use Twitter\IAM\Domain\User\Model\User;
+use Twitter\IAM\Domain\User\Model\UserRepository;
+
+final class CreateUserController
+{
+    public function __construct(
+        private UserRepository $userRepository,
+    ) {
+    }
+
+    public function __invoke(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), associative: true);
+
+        if (!isset($data['email'], $data['password'])) {
+            return new JsonResponse(['error' => 'Email and password are required'], Response::HTTP_BAD_REQUEST);
+        }
+
+        try {
+            $email = Email::fromString($data['email']);
+
+            if (null !== $this->userRepository->findByEmail($email)) {
+                return new JsonResponse(['error' => 'User with this email already exists'], Response::HTTP_CONFLICT);
+            }
+
+            $passwordHash = PasswordHash::fromPlainPassword($data['password']);
+            $user = User::create($email, $passwordHash);
+
+            $this->userRepository->save($user);
+
+            return new JsonResponse([
+                'id' => $user->getId()->toString(),
+            ], Response::HTTP_CREATED);
+        } catch (\InvalidArgumentException $argumentException) {
+            return new JsonResponse([
+                'error' => 'Invalid email or password',
+            ], Response::HTTP_BAD_REQUEST);
+        } catch (\Throwable $throwable) {
+            return new JsonResponse(['error' => 'Unexpected error'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
