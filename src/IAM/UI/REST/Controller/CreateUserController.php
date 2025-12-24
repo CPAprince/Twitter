639badc384534bac110fe1726e57cc60ad7a1308
Artem Danylov <145837590+adanyl0v@users.noreply.github.com>
2025-12-24 16:55:55 +0000

    refactor(iam): SCRUM-51 Use attributes for API routes

--- src/IAM/UI/REST/Controller/CreateUserController.php
+++ src/IAM/UI/REST/Controller/CreateUserController.php
@@ -7,11 +7,13 @@ namespace Twitter\IAM\UI\REST\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\Routing\Attribute\Route;
 use Twitter\IAM\Application\CreateUser\CreateUserCommand;
 use Twitter\IAM\Application\CreateUser\CreateUserCommandHandler;
 use Twitter\IAM\Domain\User\Exception\InvalidEmailException;
 use Twitter\IAM\Domain\User\Exception\InvalidPasswordException;
 
+#[Route('/api/users', name: 'api_create_user', methods: ['POST'])]
 final readonly class CreateUserController
 {
     public function __construct(
