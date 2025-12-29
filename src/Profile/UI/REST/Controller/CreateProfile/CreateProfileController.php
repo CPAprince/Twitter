2fe505206950f19141ca23c707647150b07bc0e8
Artem Danylov <145837590+adanyl0v@users.noreply.github.com>
2025-12-29 08:40:14 +0000

    refactor(profile): SCRUM-52 Use constants for HTTP methods

--- src/Profile/UI/REST/Controller/CreateProfile/CreateProfileController.php
+++ src/Profile/UI/REST/Controller/CreateProfile/CreateProfileController.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace Twitter\Profile\UI\REST\Controller\CreateProfile;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 use Twitter\Profile\Application\UseCase\CreateProfile\CreateProfileCommand;
 use Twitter\Profile\Application\UseCase\CreateProfile\CreateProfileCommandHandler;
 
-#[Route('/api/profiles', name: 'api_create_profile', methods: ['POST'])]
+#[Route('/api/profiles', name: 'api_create_profile', methods: [Request::METHOD_POST])]
 final readonly class CreateProfileController
 {
     public function __construct(private CreateProfileCommandHandler $handler) {}
