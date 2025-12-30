f5d55acb8ee0d4cd832fe2f5396b9739256f260e
Artem Danylov <145837590+adanyl0v@users.noreply.github.com>
2025-12-30 07:02:13 +0000

    refactor(doctrine): SCRUM-64 Add Doctrine EM reset subscriber
    Therefore, no one needs to refactor their code to fix the bug.

--- src/IAM/Infrastructure/Persistence/MySQL/Repository/MySQLUserRepository.php
+++ src/IAM/Infrastructure/Persistence/MySQL/Repository/MySQLUserRepository.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace Twitter\IAM\Infrastructure\Persistence\MySQL\Repository;
 
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
-use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
 use Twitter\IAM\Domain\User\Exception\UserAlreadyExistsException;
 use Twitter\IAM\Domain\User\Model\User;
 use Twitter\IAM\Domain\User\Model\UserRepository;
 
 final readonly class MySQLUserRepository implements UserRepository
 {
-    public function __construct(private ManagerRegistry $registry) {}
+    public function __construct(private EntityManagerInterface $entityManager) {}
 
     /**
      * @throws UserAlreadyExistsException
@@ -20,9 +20,8 @@ final readonly class MySQLUserRepository implements UserRepository
     public function add(User $user): void
     {
         try {
-            $manager = $this->registry->getManagerForClass(User::class);
-            $manager->persist($user);
-            $manager->flush();
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
         } catch (UniqueConstraintViolationException) {
             throw new UserAlreadyExistsException($user->email());
         }
