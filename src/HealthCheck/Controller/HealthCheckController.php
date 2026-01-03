9eea480e26310939f52674e83a7a63ddd37bf6c5
Artem Danylov <145837590+adanyl0v@users.noreply.github.com>
2026-01-03 17:11:34 +0000

    refactor(health): SCRUM-56 Remove unrelated response data

--- src/HealthCheck/Controller/HealthCheckController.php
+++ src/HealthCheck/Controller/HealthCheckController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Twitter\HealthCheck\Controller;
 
 use DateTimeImmutable;
-use DateTimeInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,9 +15,7 @@ final class HealthCheckController
     {
         return new JsonResponse([
             'status' => 'ok',
-            'checkedAt' => new DateTimeImmutable()->format(DateTimeInterface::RFC3339),
-            'RANDOM' => rand(1000000000, 9999999999),
-            'UUIDrandom' => 'RAND: '.uuid_create(),
+            'checkedAt' => new DateTimeImmutable()->format(DATE_RFC3339),
         ]);
     }
 }
