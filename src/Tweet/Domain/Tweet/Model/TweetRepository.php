5709830838c449b539211080c528f2098bdda8db
Artem Danylov <145837590+adanyl0v@users.noreply.github.com>
2026-01-05 19:14:44 +0000

    feat(tweet): SCRUM-57 Add methods to save and retrieve tweets by ID

--- src/Tweet/Domain/Tweet/Model/TweetRepository.php
+++ src/Tweet/Domain/Tweet/Model/TweetRepository.php
@@ -7,4 +7,8 @@ namespace Twitter\Tweet\Domain\Tweet\Model;
 interface TweetRepository
 {
     public function add(Tweet $tweet): void;
+
+    public function save(Tweet $tweet): void;
+
+    public function getById(string $tweetId): Tweet;
 }
--- src/Tweet/Infrastructure/Persistence/MySQL/Repository/MySQLTweetRepository.php
+++ src/Tweet/Infrastructure/Persistence/MySQL/Repository/MySQLTweetRepository.php
@@ -6,6 +6,9 @@ namespace Twitter\Tweet\Infrastructure\Persistence\MySQL\Repository;
 
 use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+use Twitter\Tweet\Domain\Tweet\Exception\TweetNotFoundException;
 use Twitter\Tweet\Domain\Tweet\Exception\UserNotFoundException;
 use Twitter\Tweet\Domain\Tweet\Model\Tweet;
 use Twitter\Tweet\Domain\Tweet\Model\TweetRepository;
@@ -25,5 +28,25 @@ final readonly class MySQLTweetRepository implements TweetRepository
         } catch (ForeignKeyConstraintViolationException) {
             throw new UserNotFoundException($tweet->userId());
         }
+    }
+
+    public function save(Tweet $tweet): void
+    {
+        $this->entityManager->flush();
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws TweetNotFoundException
+     * @throws ORMException
+     */
+    public function getById(string $tweetId): Tweet
+    {
+        $tweet = $this->entityManager->find(Tweet::class, $tweetId);
+        if (null === $tweet) {
+            throw new TweetNotFoundException($tweetId);
+        }
+
+        return $tweet;
     }
 }
