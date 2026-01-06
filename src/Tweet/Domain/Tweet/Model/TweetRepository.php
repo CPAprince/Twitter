d2b62e3985fdbc8c0ecc880389a4409c85ed6364
Serhii Grinenko <47193901+SergeyGrinenko@users.noreply.github.com>
2026-01-06 13:41:47 +0200

    feat(tweet-domain): SCRUM-58 add Tweet aggregate and repository contract
    Define Tweet domain model, repository contract, and domain-level exception.

---
+++ src/Tweet/Domain/Tweet/Model/TweetRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Twitter\Tweet\Domain\Tweet\Model;
+
+use Twitter\Tweet\Domain\Tweet\Exception\TweetNotFoundException;
+
+interface TweetRepository
+{
+    /**
+     * @throws TweetNotFoundException
+     */
+    public function getById(string $tweetId): Tweet;
+}
