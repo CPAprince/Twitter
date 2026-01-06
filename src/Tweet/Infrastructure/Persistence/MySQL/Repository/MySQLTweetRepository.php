160045a93bc4159d0a1bec554a6d56c66d7a6fff
Serhii Grinenko <47193901+SergeyGrinenko@users.noreply.github.com>
2026-01-06 13:43:27 +0200

    feat(tweet-persistence): SCRUM-58 add Doctrine mapping, MySQL repository and migration
    Persist Tweet aggregate using Doctrine ORM and MySQL.

---
+++ src/Tweet/Infrastructure/Persistence/MySQL/Repository/MySQLTweetRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Twitter\Tweet\Infrastructure\Persistence\MySQL\Repository;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Twitter\Tweet\Domain\Tweet\Exception\TweetNotFoundException;
+use Twitter\Tweet\Domain\Tweet\Model\Tweet;
+use Twitter\Tweet\Domain\Tweet\Model\TweetRepository;
+
+final readonly class MySQLTweetRepository implements TweetRepository
+{
+    public function __construct(private EntityManagerInterface $entityManager) {}
+
+    public function getById(string $tweetId): Tweet
+    {
+        /** @var Tweet|null $tweet */
+        $tweet = $this->entityManager->find(Tweet::class, $tweetId);
+
+        if (null === $tweet) {
+            throw new TweetNotFoundException($tweetId);
+        }
+
+        return $tweet;
+    }
+}
