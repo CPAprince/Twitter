8f70c08e845760ef3f65b7819c1102be2705fe14
Artem Danylov <145837590+adanyl0v@users.noreply.github.com>
2026-01-06 16:29:03 +0000

    feat(profile): SCRUM-60 Add `getByUserId` repository method

--- src/Profile/Domain/Profile/Model/ProfileRepository.php
+++ src/Profile/Domain/Profile/Model/ProfileRepository.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace Twitter\Profile\Domain\Profile\Model;
 
+use Twitter\Profile\Domain\Profile\Exception\ProfileNotFoundException;
+
 interface ProfileRepository
 {
     public function add(Profile $profile);
 
+    /**
+     * @throws ProfileNotFoundException
+     */
     public function getByUserId(string $userId): Profile;
 }
