c6532536e0af1c459a0da2314ccb29d0e164fe1a
Artem Danylov <145837590+adanyl0v@users.noreply.github.com>
2026-01-07 13:05:33 +0000

    refactor(tweet): SCRUM-57 Remove unused Security import

--- src/Tweet/UI/REST/UpdateTweet/UpdateTweetController.php
+++ src/Tweet/UI/REST/UpdateTweet/UpdateTweetController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Twitter\Tweet\UI\REST\UpdateTweet;
 
 use Assert\Assert;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
