96bdaaa32de76908a641b13283867980bf156fa3
Artem Danylov <145837590+adanyl0v@users.noreply.github.com>
2026-01-12 07:18:27 +0000

    test(tweet): SCRUM-57 Rename test method for clarity on ownership

--- tests/Tweet/Application/UseCase/UpdateTweet/UpdateTweetCommandHandlerTest.php
+++ tests/Tweet/Application/UseCase/UpdateTweet/UpdateTweetCommandHandlerTest.php
@@ -71,7 +71,7 @@ final class UpdateTweetCommandHandlerTest extends TestCase
      * @throws TweetNotFoundException
      */
     #[Test]
-    public function throwsAccessDeniedWhenUserIsNotOwner(): void
+    public function throwsExceptionWhenTweetIsUpdatedNotByTheOwner(): void
     {
         $tweet = Tweet::create(
             '123e4567-e89b-12d3-a456-426614174000',
