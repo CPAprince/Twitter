a8a3a14a98aa11d3198ecf8fd5c0b0554e7b8be7
illia-m-b <214269542+illia-m-b@users.noreply.github.com>
2026-01-13 10:57:20 +0200

    feat(tweet): SCRUM-63 Add `getById()` method to the Tweet Repository

--- src/Tweet/Infrastructure/Persistence/MySQL/Repository/MySQLTweetRepository.php
+++ src/Tweet/Infrastructure/Persistence/MySQL/Repository/MySQLTweetRepository.php
@@ -6,6 +6,7 @@ namespace Twitter\Tweet\Infrastructure\Persistence\MySQL\Repository;
 
 use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
 use Doctrine\ORM\EntityManagerInterface;
+use Twitter\Tweet\Domain\Tweet\Exception\TweetNotFoundException;
 use Twitter\Tweet\Domain\Tweet\Exception\UserNotFoundException;
 use Twitter\Tweet\Domain\Tweet\Model\Tweet;
 use Twitter\Tweet\Domain\Tweet\Model\TweetRepository;
@@ -25,5 +26,17 @@ final readonly class MySQLTweetRepository implements TweetRepository
         } catch (ForeignKeyConstraintViolationException) {
             throw new UserNotFoundException($tweet->userId());
         }
+    }
+
+    public function getById(string $tweetId): Tweet
+    {
+        /** @var Tweet|null $tweet */
+        $tweet = $this->entityManager->find(Tweet::class, $tweetId);
+
+        if (null === $tweet) {
+            throw new TweetNotFoundException($tweetId);
+        }
+
+        return $tweet;
     }
 }
