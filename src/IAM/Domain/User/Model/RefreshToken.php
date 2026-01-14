5627d4a30d75129fd8ed6072ee958a27dd65069c
CPAprince <40248409+CPAprince@users.noreply.github.com>
2026-01-14 10:34:45 +0200

    ORM configuration resolve duplication

--- src/IAM/Domain/User/Model/RefreshToken.php
+++ src/IAM/Domain/User/Model/RefreshToken.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Twitter\IAM\Domain\User\Model;
 
-use Doctrine\ORM\Mapping as ORM;
 use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken as BaseRefreshToken;
 
-#[ORM\Entity]
-#[ORM\Table(name: 'refresh_tokens')]
 class RefreshToken extends BaseRefreshToken {}
