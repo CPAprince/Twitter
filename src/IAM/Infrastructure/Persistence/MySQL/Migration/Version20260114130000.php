9f4470ffd2f741b4226670f4c413a5b4a1971189
Artem Danylov <145837590+adanyl0v@users.noreply.github.com>
2026-01-14 17:56:09 +0000

    refactor(iam): SCRUM-59 Remove redundant migration comments

--- src/IAM/Infrastructure/Persistence/MySQL/Migration/Version20260114130000.php
+++ src/IAM/Infrastructure/Persistence/MySQL/Migration/Version20260114130000.php
@@ -16,21 +16,19 @@ final class Version20260114130000 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        //        $this->addSql('DROP TABLE IF EXISTS refresh_tokens');
-
         $this->addSql(
             <<<SQL
-                CREATE TABLE refresh_tokens (
-                    id INT AUTO_INCREMENT NOT NULL,
-                    refresh_token VARCHAR(128) NOT NULL,
-                    username VARCHAR(255) NOT NULL,
-                    valid DATETIME NOT NULL,
-                    UNIQUE INDEX UNIQ_refresh_token (refresh_token),
-                    INDEX IDX_username (username),
-                    INDEX IDX_valid (valid),
-                    PRIMARY KEY(id)
-                ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci
-                SQL
+            CREATE TABLE refresh_tokens (
+                id INT AUTO_INCREMENT NOT NULL,
+                refresh_token VARCHAR(128) NOT NULL,
+                username VARCHAR(255) NOT NULL,
+                valid DATETIME NOT NULL,
+                UNIQUE INDEX UNIQ_refresh_token (refresh_token),
+                INDEX IDX_username (username),
+                INDEX IDX_valid (valid),
+                PRIMARY KEY(id)
+            ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci
+            SQL
         );
     }
 
