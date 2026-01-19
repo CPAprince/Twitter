bf046d4c5a3428a1f0a59307ef92e6dbecd5e7cc
Artem Danylov <145837590+adanyl0v@users.noreply.github.com>
2026-01-19 09:08:58 +0000

    feat(tweet): SCRUM-61 Implement get tweets by user ID API

---
+++ src/Tweet/Application/UseCase/GetUserTweets/GetUserTweetsQueryHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Twitter\Tweet\Application\UseCase\GetUserTweets;
+
+use Twitter\Tweet\Domain\Tweet\Model\Tweet;
+use Twitter\Tweet\Domain\Tweet\Model\TweetRepository;
+
+final readonly class GetUserTweetsQueryHandler
+{
+    public function __construct(private TweetRepository $tweetRepository) {}
+
+    public function handle(GetUserTweetsQuery $query): GetUserTweetsQueryResult
+    {
+        $tweets = $this->tweetRepository->getAllTweets();
+        $tweets = array_filter($tweets, fn (Tweet $tweet) => $tweet->userId() === $query->userId);
+
+        $chunkLength = $query->page * $query->limit;
+        if ($query->limit > 0 && $query->page >= 0 && $chunkLength <= count($tweets)) {
+            $tweets = array_slice($tweets, $chunkLength, $query->limit);
+        }
+
+        return new GetUserTweetsQueryResult($tweets);
+    }
+}
