d645969da05e1e6689ce8242a9b1d6daa358dd27
Serhii Grinenko <47193901+SergeyGrinenko@users.noreply.github.com>
2026-01-20 14:35:43 +0200

    feat(SCRUM-68): add profile tweets Twig fragment endpoint

---
+++ src/Tweet/UI/Web/Profile/Tweets/ProfileTweetsController.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Twitter\Tweet\UI\Web\Profile\Tweets;
+
+use Assert\Assert;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Twitter\Profile\Application\UseCase\GetProfile\GetProfileQuery;
+use Twitter\Profile\Application\UseCase\GetProfile\GetProfileQueryHandler;
+use Twitter\Profile\Domain\Profile\Exception\ProfileNotFoundException;
+use Twitter\Tweet\Application\UseCase\GetUserTweets\GetUserTweetsQuery;
+use Twitter\Tweet\Application\UseCase\GetUserTweets\GetUserTweetsQueryHandler;
+
+#[Route('/profiles/{userId}/tweets/fragment', name: 'profile_tweets_fragment', methods: ['GET'])]
+final class ProfileTweetsController extends AbstractController
+{
+    public function __construct(
+        private readonly GetProfileQueryHandler $profileQueryHandler,
+        private readonly GetUserTweetsQueryHandler $tweetsQueryHandler,
+    ) {}
+
+    /**
+     * Returns ONLY tweets list HTML (Twig fragment) for AJAX refresh.
+     *
+     * @throws ProfileNotFoundException
+     */
+    public function __invoke(string $userId): Response
+    {
+        Assert::that($userId)->uuid();
+
+        $profile = $this->profileQueryHandler->handle(new GetProfileQuery($userId));
+        $result = $this->tweetsQueryHandler->handle(new GetUserTweetsQuery($userId, limit: 50, page: 0));
+
+        $tweets = [];
+        foreach ($result->tweets ?? [] as $tweet) {
+            $tweets[] = [
+                'id' => $tweet->id(),
+                'content' => $tweet->content(),
+                'createdAt' => $tweet->createdAt(),
+                'updatedAt' => $tweet->updatedAt(),
+                'author' => [
+                    'id' => $tweet->userId(),
+                    'name' => $profile->name,
+                ],
+                'likes' => $tweet->likes(),
+            ];
+        }
+
+        return $this->render('component/_tweet_list.html.twig', [
+            'tweets' => $tweets,
+            'title' => 'Tweets:',
+            'showEdit' => true,
+        ]);
+    }
+}
