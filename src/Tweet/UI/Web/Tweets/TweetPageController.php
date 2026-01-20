2215e4e0f396c4b90e884d8f77ab241dd3af07e8
CPAprince <40248409+CPAprince@users.noreply.github.com>
2026-01-20 17:34:40 +0200

    Tidy

--- src/Tweet/UI/Web/Tweets/TweetPageController.php
+++ src/Tweet/UI/Web/Tweets/TweetPageController.php
@@ -4,19 +4,14 @@ declare(strict_types=1);
 
 namespace Twitter\Tweet\UI\Web\Tweets;
 
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Twig\Environment;
-use Twig\Error\LoaderError;
-use Twig\Error\RuntimeError;
-use Twig\Error\SyntaxError;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-
-class TweetPageController extends AbstractController{
+class TweetPageController extends AbstractController
+{
     #[Route('/t/{tweetId}', name: 'TweetPage', methods: ['GET'])]
-public function showTweetPage(string $tweetId) : Response
+    public function showTweetPage(string $tweetId): Response
     {
         return $this->render('page/tweetpage.html.twig');
     }
