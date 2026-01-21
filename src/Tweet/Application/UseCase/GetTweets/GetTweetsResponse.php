3afdfddea6f6b4f887e9fc2744c1da6871cd4c10
illia-m-b <214269542+illia-m-b@users.noreply.github.com>
2026-01-21 12:15:56 +0200

    feat(application): SCRUM-72 Update DTOs with pagination fields

--- src/Tweet/Application/UseCase/GetTweets/GetTweetsResponse.php
+++ src/Tweet/Application/UseCase/GetTweets/GetTweetsResponse.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace Twitter\Tweet\Application\UseCase\GetTweets;
 
+use Twitter\Tweet\Application\UseCase\Shared\PaginationMeta;
 use Twitter\Tweet\Application\UseCase\Shared\TweetResponse;
 
 final readonly class GetTweetsResponse
 {
     /** @param list<TweetResponse> $tweets */
-    public function __construct(public array $tweets) {}
+    public function __construct(
+        public array $tweets,
+        public PaginationMeta $meta,
+    ) {}
 }
