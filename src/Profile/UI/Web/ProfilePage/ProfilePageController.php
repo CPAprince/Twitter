24dc9f843671bf33d43926f09d862978571c5cfd
Serhii Grinenko <47193901+SergeyGrinenko@users.noreply.github.com>
2026-01-21 13:01:36 +0200

    feat(SCRUM-61): render homepage and profile tweets via API instead of SSR

--- src/Profile/UI/Web/ProfilePage/ProfilePageController.php
+++ src/Profile/UI/Web/ProfilePage/ProfilePageController.php
@@ -11,15 +11,12 @@ use Symfony\Component\Routing\Attribute\Route;
 use Twitter\Profile\Application\UseCase\GetProfile\GetProfileQuery;
 use Twitter\Profile\Application\UseCase\GetProfile\GetProfileQueryHandler;
 use Twitter\Profile\Domain\Profile\Exception\ProfileNotFoundException;
-use Twitter\Tweet\Application\UseCase\GetUserTweets\GetUserTweetsQuery;
-use Twitter\Tweet\Application\UseCase\GetUserTweets\GetUserTweetsQueryHandler;
 
 #[Route('/profiles/{userId}', name: 'profile_page', methods: ['GET'])]
 final class ProfilePageController extends AbstractController
 {
     public function __construct(
         private readonly GetProfileQueryHandler $profileQueryHandler,
-        private readonly GetUserTweetsQueryHandler $tweetsQueryHandler,
     ) {}
 
     /**
@@ -29,31 +26,7 @@ final class ProfilePageController extends AbstractController
     {
         Assert::that($userId)->uuid();
 
-        // Fetch profile
-        $profileQuery = new GetProfileQuery($userId);
-        $profileResult = $this->profileQueryHandler->handle($profileQuery);
-
-        // Fetch user tweets
-        $tweetsQuery = new GetUserTweetsQuery($userId, limit: 50, page: 0);
-        $tweetsResult = $this->tweetsQueryHandler->handle($tweetsQuery);
-
-        // Map tweets to array format for template
-        $tweets = [];
-        if (!empty($tweetsResult->tweets)) {
-            foreach ($tweetsResult->tweets as $tweet) {
-                $tweets[] = [
-                    'id' => $tweet->id(),
-                    'content' => $tweet->content(),
-                    'createdAt' => $tweet->createdAt(),
-                    'updatedAt' => $tweet->updatedAt(),
-                    'author' => [
-                        'id' => $tweet->userId(),
-                        'name' => $profileResult->name,
-                    ],
-                    'likes' => $tweet->likes(),
-                ];
-            }
-        }
+        $profileResult = $this->profileQueryHandler->handle(new GetProfileQuery($userId));
 
         return $this->render('page/profile.html.twig', [
             'profile' => [
@@ -63,7 +36,6 @@ final class ProfilePageController extends AbstractController
                 'createdAt' => $profileResult->createdAt,
                 'updatedAt' => $profileResult->updatedAt,
             ],
-            'tweets' => $tweets,
         ]);
     }
 }
--- src/Tweet/UI/Web/Homepage/HomepageController.php
+++ src/Tweet/UI/Web/Homepage/HomepageController.php
@@ -7,38 +7,12 @@ namespace Twitter\Tweet\UI\Web\Homepage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Twitter\Tweet\Application\UseCase\GetTweets\GetTweetsCommand;
-use Twitter\Tweet\Application\UseCase\GetTweets\GetTweetsCommandHandler;
-use Twitter\Tweet\Application\UseCase\Shared\TweetResponse;
 
 final class HomepageController extends AbstractController
 {
-    public function __construct(
-        private readonly GetTweetsCommandHandler $handler,
-    ) {}
-
     #[Route('/', name: 'homepage', methods: ['GET'])]
     public function __invoke(): Response
     {
-        $dto = $this->handler->handle(new GetTweetsCommand());
-
-        $tweets = array_map(
-            static fn (TweetResponse $tweet): array => [
-                'id' => $tweet->id,
-                'content' => $tweet->content,
-                'createdAt' => $tweet->createdAt,
-                'updatedAt' => $tweet->updatedAt,
-                'author' => [
-                    'id' => $tweet->authorId,
-                    'name' => $tweet->authorName,
-                ],
-                'likes' => $tweet->likesCount,
-            ],
-            $dto->tweets
-        );
-
-        return $this->render('page/homepage.html.twig', [
-            'tweets' => $tweets,
-        ]);
+        return $this->render('page/homepage.html.twig');
     }
 }
