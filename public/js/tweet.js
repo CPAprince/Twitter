684faca504131d9f8fec383c78afab7a571f668a
CPAprince <40248409+CPAprince@users.noreply.github.com>
2026-01-23 19:59:37 +0200

    Fixes Single Tweet Page editing functional added

--- public/js/tweet.js
+++ public/js/tweet.js
@@ -61,10 +61,21 @@ function showTweet(tweet) {
   console.log('Tweet JSON data in show:', tweet);
   document.querySelector('[tweet_author]').innerHTML = '<a href=\"\/p\/' + tweet.author.id + '\"  style=\"color: var(--bs-body-color);\">' + tweet.author.name + '</a>';
   document.querySelector('[tweet_createdAt]').textContent = ' · ' + formatPublishDate(tweet.createdAt);
-  document.querySelector('[tweet_content]').textContent = tweet.content;
+  document.querySelector('[data-tweet-content]').textContent = tweet.content;
   document.querySelector('[tweet_likes]').textContent = tweet.likesCount;
   document.querySelector(" div.tweet-actions > div > button.tweet-like-btn").setAttribute('data-tweet-id', tweet.id);
-  // document.querySelector(" div.tweet-actions > div > button.tweet-like-btn").setAttribute('data-tweet-id', tweet.id);
-  // document.querySelector(" div.tweet-actions > div > button.tweet-like-btn").setAttribute('data-author-id', tweet.author.id);
+  document.querySelector("div > div.tweet-edit-form > div").setAttribute('data-tweet-id', tweet.id);
+  document.querySelector("div > button.btn.tweet-edit-btn").setAttribute('data-tweet-id', tweet.id);
+  document.querySelector("div > button.btn.tweet-edit-btn").setAttribute('data-author-id', tweet.author.id);
+
   document.querySelector('.tweet').style.visibility = 'visible';
+
+  window.Auth.getCurrentUserId().then(function(userId) {
+    //console.log(userId); //
+    if(userId == tweet.author.id) {
+      document.querySelector("button.tweet-edit-btn").style.display = 'inline-block';
+    }
+  });
+
+
 }
