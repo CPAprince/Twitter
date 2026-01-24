9952227a442dbab43fdc02f98ced600102206d5f
CPAprince <40248409+CPAprince@users.noreply.github.com>
2026-01-25 00:45:59 +0200

    Dataset User Tweets Pagination using MySQL

--- src/Tweet/Application/UseCase/GetUserTweets/GetUserTweetsQueryHandler.php
+++ src/Tweet/Application/UseCase/GetUserTweets/GetUserTweetsQueryHandler.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Twitter\Tweet\Application\UseCase\GetUserTweets;
 
-use Twitter\Tweet\Domain\Tweet\Model\Tweet;
 use Twitter\Tweet\Domain\Tweet\Model\TweetRepository;
 
 final readonly class GetUserTweetsQueryHandler
@@ -14,6 +13,7 @@ final readonly class GetUserTweetsQueryHandler
     public function handle(GetUserTweetsQuery $query): GetUserTweetsQueryResult
     {
         $tweets = $this->tweetRepository->getAllTweets($query->limit, $query->page, $query->userId);
+
         return new GetUserTweetsQueryResult($tweets);
     }
 }
