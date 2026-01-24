17ce17b19070a47860ead28881f6da93949b5ab3
CPAprince <40248409+CPAprince@users.noreply.github.com>
2026-01-25 00:44:49 +0200

    Dataset User Tweets Pagination using MySQL

--- src/Tweet/Application/UseCase/GetUserTweets/GetUserTweetsQueryHandler.php
+++ src/Tweet/Application/UseCase/GetUserTweets/GetUserTweetsQueryHandler.php
@@ -14,14 +14,6 @@ final readonly class GetUserTweetsQueryHandler
     public function handle(GetUserTweetsQuery $query): GetUserTweetsQueryResult
     {
         $tweets = $this->tweetRepository->getAllTweets($query->limit, $query->page, $query->userId);
-
-        $tweets = array_filter($tweets, fn (Tweet $tweet) => $tweet->userId() === $query->userId);
-
-        $chunkLength = $query->page * $query->limit;
-        if ($query->limit > 0 && $query->page >= 0 && $chunkLength <= count($tweets)) {
-            $tweets = array_slice($tweets, $chunkLength, $query->limit);
-        }
-
         return new GetUserTweetsQueryResult($tweets);
     }
 }
