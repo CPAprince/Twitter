a3d04ea8bce4c9e307e21849e18f59aa07c28df6
CPAprince <40248409+CPAprince@users.noreply.github.com>
2026-01-25 04:18:18 +0200

    /api/tweets?limit=100&page=1
    API pagination and limit for General Feed

--- src/Tweet/Application/UseCase/GetTweets/GetTweetsCommand.php
+++ src/Tweet/Application/UseCase/GetTweets/GetTweetsCommand.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 
 namespace Twitter\Tweet\Application\UseCase\GetTweets;
 
-final readonly class GetTweetsCommand {
-
+final readonly class GetTweetsCommand
+{
     public function __construct(
-        public int $limit,
-        public int $page,
-    ){}
+        public int $limit = 100,
+        public int $page = 1,
+    ) {}
 }
-
