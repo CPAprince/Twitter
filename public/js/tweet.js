7eb8b1670f06ad9ae1d2f4727a49376859786126
Artem Danylov <145837590+adanyl0v@users.noreply.github.com>
2026-01-27 08:11:02 +0000

    fix(tweet): SCRUM-98 Target elements by their unique class names

--- public/js/tweet.js
+++ public/js/tweet.js
@@ -68,10 +68,10 @@ function showTweet(tweet) {
   document.querySelector('[tweet_createdAt]').textContent = ' · ' + formatPublishDate(tweet.createdAt);
   document.querySelector('[data-tweet-content]').textContent = tweet.content;
   document.querySelector('[tweet_likes]').textContent = tweet.likesCount;
-  document.querySelector("div.tweet-actions > div > button.tweet-like-btn").setAttribute('data-tweet-id', tweet.id);
-  document.querySelector("div > div.tweet-edit-form > div").setAttribute('data-tweet-id', tweet.id);
-  document.querySelector("div > button.btn.tweet-edit-btn").setAttribute('data-tweet-id', tweet.id);
-  document.querySelector("div > button.btn.tweet-edit-btn").setAttribute('data-author-id', tweet.author.id);
+  document.querySelector(".tweet-like-btn").setAttribute('data-tweet-id', tweet.id);
+  document.querySelector(".tweet-form-container").setAttribute('data-tweet-id', tweet.id);
+  document.querySelector(".tweet-edit-btn").setAttribute('data-tweet-id', tweet.id);
+  document.querySelector(".tweet-edit-btn").setAttribute('data-author-id', tweet.author.id);
 
   document.querySelector('.tweet').style.visibility = 'visible';
 
